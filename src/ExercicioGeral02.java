a100b038070ba97a738ad935a0aaab062d0910ab
Edvunge <53275378+Edvunge@users.noreply.github.com>
2021-09-22 03:26:25 +0100

    frist commit

---
+++ src/ExercicioGeral02.java
@@ -0,0 +1,53 @@
+public class ExercicioGeral02 {
+    public static void main(String[] args){
+        /*
+           Questões --------------------------------------------------------------------
+ 2.14 Escreva um aplicativo que exiba os números 1 a 4 na mesma linha, com cada par de adjacentes separados por um espaço.
+ Use as seguintes técnicas:
+a) UmainstruçãoSystem.out.println. b) Quatro instruções System.out.print. c) UmainstruçãoSystem.out.printf.
+
+
+2.15 (Aritmética) Escreva um aplicativo que solicite ao usuário inserir dois inteiros, obtenha dele esses números e imprima sua soma,
+produto, diferença e quociente (divisão). Utilize as técnicas mostradas na Figura 2.7.
+
+
+2.16 (Comparando inteiros) Escreva um aplicativo que solicite ao usuário inserir dois inteiros,
+obtenha dele esses números e exiba o número maior seguido pelas palavras “is larger".
+Se os números forem iguais, imprima a mensagem “These numbers are equal". Utilize as técnicas mostradas na Figura 2.15.
+
+
+2.17 (Aritmética, menor e maior) Escreva um aplicativo que insira três inteiros digitados pelo usuário e
+exiba a soma, média, produto e os números menores e maiores. Utilize as técnicas mostradas na Figura 2.15.
+[Observação: o cálculo da média neste exercício deve resultar em uma representação de inteiro.
+Assim, se a soma dos valores for 7, a média deverá ser 2, não 2,3333...]
+
+
+2.18 (Exibindo formas com asteriscos) Escreva um aplicativo que exiba uma caixa, uma elipse, uma seta e um losango utilizando asteriscos (*), como segue:
+2.19 O que o seguinte código imprime?
+System.out.printf("*%n**%n***%n****%n*****%n");
+
+2.20 O que o seguinte código imprime?
+System.out.println("*"); System.out.println("***");
+System.out.println("*****");
+System.out.println("****");
+System.out.println("**");
+
+
+2.21 O que o seguinte código imprime?
+System.out.print("*");
+System.out.print("***");
+System.out.print("*****");
+System.out.print("****");
+System.out.println("**");
+
+
+2.22 O que o seguinte código imprime?
+System.out.print("*");
+System.out.println("***");
+System.out.println("*****");
+System.out.print("****");
+System.out.println("**");
+
+*/
+    }
+}
